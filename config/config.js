8c9abb9c3863fdeb23cbf680c0f76bd50841f4be
somenath_ghosh <somenath.ghosh84@gmail.com>
2017-03-25 19:54:24 +0530

    init Sat Mar 25 19:54:11 IST 2017

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'chatbot'
+    },
+    port: process.env.PORT || 3000,
+    db: process.env.MONGODB_URI || 'mongodb://heroku_89ww4mwb:fv3ppltiacckiedos2nnr8kp37@ds141450.mlab.com:41450/heroku_89ww4mwb' 
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'chatbot'
+    },
+    port: process.env.PORT || 3000,
+    db: process.env.MONGODB_URI
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'chatbot'
+    },
+    port: process.env.PORT || 3000,
+    db: process.env.MONGODB_URI
+  }
+};
+
+module.exports = config[env];
---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  plumber = require('gulp-plumber'),
+  livereload = require('gulp-livereload');
+  autoprefixer = require('gulp-autoprefixer'),
+  cssnano = require('gulp-cssnano'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  notify = require('gulp-notify'),
+  cache = require('gulp-cache'),
+  del = require('del');
+
+
+gulp.task('develop', function () {
+  livereload.listen();
+  nodemon({
+    script: 'app.js',
+    ext: 'js coffee ejs',
+    stdout: false
+  }).on('readable', function () {
+    this.stdout.on('data', function (chunk) {
+      if(/^Express server listening on port/.test(chunk)){
+        livereload.changed(__dirname);
+      }
+    });
+    this.stdout.pipe(process.stdout);
+    this.stderr.pipe(process.stderr);
+  });
+});
+
+//style
+
+gulp.task('styles', function() {
+  return gulp.src('public/css/*.css')
+    .pipe(gulp.dest('public/dist/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cssnano())
+    .pipe(gulp.dest('public/dist/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+//scripts
+gulp.task('scripts', function() {
+  return gulp.src('public/js/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('public/dist/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+//images
+gulp.task('images', function() {
+  return gulp.src('public/img/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('public/dist/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+//clean
+gulp.task('clean', function() {
+    return del(['public/dist/css', 'public/dist/js', 'public/dist/img']);
+});
+
+//run
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .css files
+  gulp.watch('public/css/*.css', ['styles']);
+
+  // Watch .js files
+  gulp.watch('public/js/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('public/img/*', ['images']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['public/dist/**']).on('change', livereload.changed);
+
+});
+
+// gulp.task('default', [
+//   'develop'
+// ]);
